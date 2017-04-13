53ddee3b63781d29108430145535fd927d3156b7
Hai Phan <htp200006@gmail.com>
2017-04-13 13:29:51 -0500

    Added code to matchingActivity to change format of spinner and made spinner_item.xml

--- app/src/main/java/net/androidbootcamp/haiquizapp/matchingActivity.java
+++ app/src/main/java/net/androidbootcamp/haiquizapp/matchingActivity.java
@@ -2,6 +2,8 @@ package net.androidbootcamp.haiquizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class matchingActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class matchingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matching);
+
+        //changes the size and font color of spinner from the R.layout.spinner_item in layouts folder
+        Spinner item1 = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> item1adapter = ArrayAdapter.createFromResource(
+                this, R.array.answerGrp1, R.layout.spinner_item);
+        item1adapter.setDropDownViewResource(R.layout.spinner_item);
+        item1.setAdapter(item1adapter);
+
+        //changes the size and font color of spinner from the R.layout.spinner_item in layouts folder
+        Spinner item2 = (Spinner) findViewById(R.id.spinner3);
+        ArrayAdapter<CharSequence> item2adapter = ArrayAdapter.createFromResource(
+                this, R.array.answerGrp2, R.layout.spinner_item);
+        item2adapter.setDropDownViewResource(R.layout.spinner_item);
+        item2.setAdapter(item2adapter);
+
     }
 }
